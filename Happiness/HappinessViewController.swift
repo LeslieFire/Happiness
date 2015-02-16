eecc35553e248b3c9aae70a10dc75827384b2c29
Leslie <249893977@qq.com>
2015-02-16 13:53:53 +0800

    FaceView

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Leslie Yang on 15/2/15.
+//  Copyright (c) 2015 SeeFeel App House. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
